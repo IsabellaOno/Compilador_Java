47ef0512722fc7081435a713f6af8c9a5b6a128a
IsabellaOno <isabella.ono@aluno.ufabc.edu.br>
2024-09-09 00:20:11 -0300

    .

---
+++ src/io/compiler/core/exceptions/IsiLanguageSemanticException.java
@@ -0,0 +1,9 @@
+package io.compiler.core.exceptions;
+
+public class IsiLanguageSemanticException extends RuntimeException{
+	
+	public IsiLanguageSemanticException(String message) {
+		super(message);
+	}
+
+}
--- src/io/compiler/core/exceptions/UFABCSemanticException.java
+++
@@ -1,9 +0,0 @@
-package io.compiler.core.exceptions;
-
-public class UFABCSemanticException extends RuntimeException{
-	
-	public UFABCSemanticException(String message) {
-		super(message);
-	}
-
-}
