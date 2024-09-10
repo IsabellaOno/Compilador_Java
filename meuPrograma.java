0a67df2d5e634039ec7b1d56e6dfcb8e56d7312e
Letícia do Carmo Melo <110508442+LeticiadCM@users.noreply.github.com>
2024-09-10 02:49:27 -0300

    Update meuPrograma.java

--- meuPrograma.java
+++ meuPrograma.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
+
 public class meuPrograma{ 
+   
    public static void main(String args[]){ 
-   Scanner _scTrx = new Scanner(System.in);
-    int a;
-    int b;
-    String x;
-    String y;
-System.out.println("Hello World");
-System.out.println("Fim do programa");
-a = _scTrx.nextInt();
-b = _scTrx.nextInt();
-System.out.println(a);
-if (a+2>5-b) {System.out.println("maior que 5");
-}else {System.out.println("menor ou igual a 5");
-}   }
+      
+      Scanner _scTrx = new Scanner(System.in);
+      int a;
+      int b;
+      String x;
+      String y;
+      System.out.println("Hello World");
+      System.out.println("Fim do programa");
+      a = _scTrx.nextInt();
+      b = _scTrx.nextInt();
+      x = _scTrx.nextLine();
+      x = _scTrx.nextLine();
+      System.out.println(a);
+
+      //Falta adicionar WHILE, DO-WHILE e FOR 
+      
+      if (a+2>5-b) {
+         System.out.println("maior que 5");
+      }
+      else {
+         System.out.println("menor ou igual a 5");
+      }
+
+      
 }
