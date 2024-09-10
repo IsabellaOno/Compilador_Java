818d80cbc78e4126bba20837cc25642baa01be60
Letícia do Carmo Melo <110508442+LeticiadCM@users.noreply.github.com>
2024-09-10 02:17:24 -0300

    Update and rename ComandoSe.java to IfCommand.java

--- src/io/compiler/core/ast/IfCommand.java
+++ src/io/compiler/core/ast/IfCommand.java
@@ -2,35 +2,39 @@ package io.compiler.core.ast;
 
 import java.util.List;
 
-public class ComandoSe extends Command{
+public class IfCommand extends Command{
 
 	private String expression;
 	private List<Command> trueList;
 	private List<Command> falseList;
 	
-	public ComandoSe() {
+	public IfCommand() {
 		super();
 	}
 
-	public ComandoSe(String expression, List<Command> trueList, List<Command> falseList) {
+	public IfCommand(String expression, List<Command> trueList, List<Command> falseList) {
 		super();
 		this.expression = expression;
 		this.trueList = trueList;
 		this.falseList = falseList;
 	}
 
+	//Obter a expressão condicional
 	public String getExpression() {
 		return expression;
 	}
 
+	//Definir a expressão condicional
 	public void setExpression(String expression) {
 		this.expression = expression;
 	}
 
+	//Obter a lista de comandos
 	public List<Command> getTrueList() {
 		return trueList;
 	}
 
+	//Definir a lista de comandos
 	public void setTrueList(List<Command> trueList) {
 		this.trueList = trueList;
 	}
@@ -45,23 +49,19 @@ public class ComandoSe extends Command{
 
 	@Override
 	public String generateTarget() {
-		// TODO Auto-generated method stub
 		StringBuilder str = new StringBuilder();
-		str.append("if ("+expression+") {");
+		str.append("if("+expression+"){");
 		for (Command cmd: trueList) {
 			str.append(cmd.generateTarget());
 		}
-		str.append("}");
-		
+
 		if (!falseList.isEmpty()) {
-			str.append("else {");
+			str.append("} else {");
 			for (Command cmd: falseList) {
 				str.append(cmd.generateTarget());
 			}
-			str.append("}");
 		}
+		str.append("}");
 		return str.toString();
 	}
-	
-
 }
