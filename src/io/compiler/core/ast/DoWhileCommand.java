ce39f889b684f2c3ce7254911239842a5ad16118
Letícia do Carmo Melo <110508442+LeticiadCM@users.noreply.github.com>
2024-09-10 01:50:12 -0300

    Update and rename ComandoFacaEnquanto.java to DoWhileCommand.java

---
+++ src/io/compiler/core/ast/DoWhileCommand.java
@@ -0,0 +1,53 @@
+package io.compiler.core.ast;
+
+import java.util.List;
+
+public class DoWhileCommand extends Command {
+
+  //Atributo que armazena a expressão condicional do laço "while"
+	private String expression;
+
+  //Lista de comandos a serem executados
+	private List<Command> whileCommands;
+
+  //Obter a expressão condicional
+	public String getExpression() {
+		return expression;
+	}
+
+  //Definir a expressão condicional
+	public void setExpression(String expression) {
+		this.expression = expression;
+	}
+
+  //Obter a lista de comandos
+	public List<Command> getList() {
+		return whileCommands;
+	}
+
+  //Definir a lista de comandos
+	public void setList(List<Command> whileCommands) {
+		this.whileCommands = whileCommands;
+	}
+
+  //Recebe a expressão condicional e a lista de comandos
+	public DoWhileCommand(String expression, List<Command> whileCommands) {
+		this.expression = expression;
+		this.whileCommands = whileCommands;
+	}
+
+	public DoWhileCommand() {
+	}
+
+	@Override
+	public String generateTarget() {
+		StringBuilder str = new StringBuilder();
+		str.append("do {");
+		for (Command cmd: whileCommands) {
+			str.append(cmd.generateTarget());
+		}
+		str.append("} while (" + expression + ");\n");
+		return str.toString();
+	}
+
+}
