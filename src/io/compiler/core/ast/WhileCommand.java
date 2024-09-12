74f7ae181a5fdf19f080e708e52d9ea5bfa92d45
Letícia do Carmo Melo <110508442+LeticiadCM@users.noreply.github.com>
2024-09-11 22:53:32 -0300

    Update WhileCommand.java

--- src/io/compiler/core/ast/WhileCommand.java
+++ src/io/compiler/core/ast/WhileCommand.java
@@ -4,32 +4,32 @@ import java.util.List;
 
 public class WhileCommand extends Command {
 
-  //Atributo que armazena a expressão condicional do laço "while"
+  	//Atributo que armazena a expressão condicional do laço "while"
 	private String expression;
-  //Lista de comandos a serem executados dentro do bloco "while".
+  	//Lista de comandos a serem executados dentro do bloco "while".
 	private List<Command> whileCommands;
 
-  //Obter a expressão condicional
+  	//Obter a expressão condicional
 	public String getExpression() {
 		return expression;
 	}
 
-  //Definir a expressão condicional
+  	//Definir a expressão condicional
 	public void setExpression(String expression) {
 		this.expression = expression;
 	}
 
-  //Obter a lista de comandos
+  	//Obter a lista de comandos
 	public List<Command> getList() {
 		return whileCommands;
 	}
 
-  //Definir a lista de comandos
+  	//Definir a lista de comandos
 	public void setList(List<Command> whileCommands) {
 		this.whileCommands = whileCommands;
 	}
 
-  //Recebe a expressão condicional e a lista de comandos.
+  	//Recebe a expressão condicional e a lista de comandos.
 	public WhileCommand(String expression, List<Command> whileCommands) {
 		this.expression = expression;
 		this.whileCommands = whileCommands;
@@ -42,18 +42,14 @@ public class WhileCommand extends Command {
 	public String generateTarget() {
 		// TODO Auto-generated method stub
 		StringBuilder str = new StringBuilder();
-		str.append("while (").append(expression).append("){");
+		str.append("while ("+expression+"){");
 		for (Command cmd: whileCommands) {
-			str.append("\t").append(cmd.generateTarget());
+			str.append(cmd.generateTarget());
 		}
     
-		str.append("}\n");
+		str.append("}");
 		return str.toString();
 	}
 	
-	@Override
-    public String toString() {
-        return "WhileCommand {expression='" + expression + "', whileCommands=" + whileCommands + "}";
-    }
 }
 	
