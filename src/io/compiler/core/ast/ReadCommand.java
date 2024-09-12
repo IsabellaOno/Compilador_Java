30bd41ef59081730d85a3fa7afc4db1483fad37d
Letícia do Carmo Melo <110508442+LeticiadCM@users.noreply.github.com>
2024-09-11 23:02:23 -0300

    Update ReadCommand.java

--- src/io/compiler/core/ast/ReadCommand.java
+++ src/io/compiler/core/ast/ReadCommand.java
@@ -8,11 +8,9 @@ public class ReadCommand extends Command {
 	private Var var;
 
 	public ReadCommand() {
-		super();
 	}
 
 	public ReadCommand(Var var) {
-		super();
 		this.var = var;
 	}
 
@@ -26,8 +24,7 @@ public class ReadCommand extends Command {
 
 	@Override
 	public String generateTarget() {
-		// TODO Auto-generated method stub
-		return var.getId() + " = " + ((var.getType()==Types.NUMBER)?"_scTrx.nextInt();":"_scTrx.nextLine();")+"\n";
+		return var.getId() + "=" + ((var.getType()==Types.NUMBER)?"_scTrx.nextInt();":"_scTrx.nextLine();")+"\n";
 	}
 
 }
